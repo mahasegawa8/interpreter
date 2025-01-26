877edd45a320b625d3bb53e154418cebf391611c
mahasegawa8 <hsgw831@gmail.com>
2025-01-26 18:48:03 +0900

    fix repl

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,8 @@ import (
     "io"
 
     "github.com/mahasegawa8/interpreter/lexer"
-    "github.com/mahasegawa8/interpreter/token"
+    "github.com/mahasegawa8/interpreter/parser"
+    "github.com/mahasegawa8/interpreter/evaluator"
 )
 
 const PROMPT = ">> "
@@ -19,14 +20,29 @@ func Start(in io.Reader, out io.Writer) {
         scanned := scanner.Scan()
         if !scanned {
             return
-        }
+         }
 
         line := scanner.Text()
         l:= lexer.New(line)
+        p := parser.New(l)
 
-        for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-            fmt.Printf("%+v\n", tok)
+        program := p.ParseProgram()
+        if len(p.Errors()) != 0 {
+            printParseErrors(out, p.Errors())
+            continue
         }
+
+        evaluated := evaluator.Eval(program)
+        if evaluated != nil {
+            io.WriteString(out, evaluated.Inspect())
+            io.WriteString(out, "\n")
+        }
+    }
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+    for _, msg := range errors {
+        io.WriteString(out, "\t"+msg+"\n")
     }
 }
 
