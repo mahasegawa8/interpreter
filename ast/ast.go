e08cc8eabac11eb27ded1a45756084123d01fdf1
mahasegawa8 <hsgw831@gmail.com>
2025-01-29 15:31:32 +0900

    add string parse test

--- ast/ast.go
+++ ast/ast.go
@@ -215,3 +215,12 @@ type FunctionLiteral struct {
     Body        *BlockStatement
 }
 
+type StringLiteral struct {
+    Token   token.Token
+    Value   string
+}
+
+func (sl *StringLiteral) expressionNode()      {}
+func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
+func (sl *StringLiteral) String() string       { return sl.Token.Literal }
+
